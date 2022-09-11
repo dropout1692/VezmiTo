6728f6048f3b0c2f53100c2dbe3cf8da8731d35a
michalpalenkar <michalpalenkar@gmail.com>
2022-09-11 08:59:47 +0200

    map integration *

--- web/src/layout/PageContainer.tsx
+++ web/src/layout/PageContainer.tsx
@@ -5,30 +5,32 @@ export function PageContainer({ children }: { children: JSX.Element }) {
   const router = useRouter()
   return (
     <div>
-      <Button
-        onClick={() => router.pushRoute('home')}
-        className="bg-blue-500 hover:bg-blue-600 transition m-2"
-      >
-        Home
-      </Button>
-      <Button
-        onClick={() => router.pushRoute('adminDashboard')}
-        className="bg-blue-500 hover:bg-blue-600 transition m-2"
-      >
-        Admin dashboard
-      </Button>
-      <Button
-        onClick={() => router.pushRoute('privacyPolicy')}
-        className="bg-blue-500 hover:bg-blue-600 transition m-2"
-      >
-        Privacy policy
-      </Button>
-      <Button
-        onClick={() => router.pushRoute('aboutProject')}
-        className="bg-blue-500 hover:bg-blue-600 transition m-2"
-      >
-        About project
-      </Button>
+      <div className="fixed z-10 right-0">
+        <Button
+          onClick={() => router.pushRoute('home')}
+          className="bg-blue-500 hover:bg-blue-600 transition m-2"
+        >
+          Home
+        </Button>
+        <Button
+          onClick={() => router.pushRoute('adminDashboard')}
+          className="bg-blue-500 hover:bg-blue-600 transition m-2"
+        >
+          Admin dashboard
+        </Button>
+        <Button
+          onClick={() => router.pushRoute('privacyPolicy')}
+          className="bg-blue-500 hover:bg-blue-600 transition m-2"
+        >
+          Privacy policy
+        </Button>
+        <Button
+          onClick={() => router.pushRoute('aboutProject')}
+          className="bg-blue-500 hover:bg-blue-600 transition m-2"
+        >
+          About project
+        </Button>
+      </div>
       {children}
     </div>
   )
--- web/src/pages/home/HomePage.tsx
+++ web/src/pages/home/HomePage.tsx
@@ -1,3 +1,21 @@
+import Leaflet from 'leaflet'
+import { useEffect } from 'react'
+import 'leaflet/dist/leaflet.css'
+
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Home page</h1>
+  useEffect(() => {
+    var map = Leaflet.map('map').setView([51.505, -0.09], 13)
+    Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map)
+    return () => {
+      map.remove()
+    }
+  }, [])
+  return (
+    <div>
+      <div id="map" className="h-screen w-screen z-0"></div>
+    </div>
+  )
 }
