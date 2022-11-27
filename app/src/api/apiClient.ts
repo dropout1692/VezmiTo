7824ca88e558e50aee1e6e79bca15aac83b42865
michalpalenkar <michalpalenkar@gmail.com>
2022-11-27 20:00:32 +0100

    app api client

---
+++ app/src/api/apiClient.ts
@@ -0,0 +1,44 @@
+import axiosInstance from "./axiosInstance";
+import { dispatchType } from "../store/storeInstance";
+import { UI } from "../store/redux/constants";
+
+export type RequestMethodType = "get" | "post";
+
+let requestCounter = 0;
+
+export function apiClient({ customConfig = {} } = {}) {
+  return {
+    request({
+      url,
+      method = "get",
+      data,
+    }: {
+      url: string;
+      method?: RequestMethodType;
+      data?: Record<string, any>;
+    }) {
+      if (requestCounter === 0) {
+        dispatchType(UI.SET_LOADING_START);
+      }
+      return axiosInstance({
+        url,
+        method,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        ...(data && { data }),
+        ...customConfig,
+      })
+        .then((res) => res.data)
+        .finally(() => {
+          if (requestCounter >= 1) {
+            requestCounter -= 1;
+          }
+
+          if (requestCounter === 0) {
+            dispatchType(UI.SET_LOADING_STOP);
+          }
+        });
+    },
+  };
+}
