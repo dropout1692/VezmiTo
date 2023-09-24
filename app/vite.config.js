7d1ef662cf2467c1d688549c6a01fbec4627fd07
michalpalenkar <michalpalenkar@gmail.com>
2023-09-24 18:54:31 +0200

    feat(root): add github pages deploy script

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   build: {
     manifest: true,
     rollupOptions: {
       input: './index.html',
       output: {
-        dir: 'build'
+        dir: 'build',
       },
     },
   },
