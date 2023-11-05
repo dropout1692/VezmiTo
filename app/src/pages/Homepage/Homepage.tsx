6c8c772d00e0cd12017c12e46172f1df869943e1
michalpalenkar <michalpalenkar@gmail.com>
2023-11-05 15:45:18 +0100

    feat(app): set location and zoom on init

--- app/src/pages/Homepage/Homepage.tsx
+++ app/src/pages/Homepage/Homepage.tsx
@@ -11,11 +11,19 @@ import {
   useSubmissionSelector,
   useSubmissionsDispatch,
 } from './../../store/features/submissions/submissionsSlice'
+import stringify from 'json-stringify-safe'
+import { getUrlParameters } from '../../libs/tools/urlParams'
+import {
+  setZoomAndLocation,
+  usePageDispatch,
+} from '../../store/features/page/pageSlice'
 
 export const Homepage = () => {
+  const { lat, lng, zoom } = getUrlParameters()
   const submissionsStatus = useSubmissionSelector(selectSubmissionsStatus)
   const submissions = useSubmissionSelector(selectAllSubmissions)
   const submissionsDispatch = useSubmissionsDispatch()
+  const pageDispatch = usePageDispatch()
 
   const [showAddSubmissionModal, setShowAddSubmissionModal] =
     useState<boolean>(false)
@@ -32,6 +40,32 @@ export const Homepage = () => {
     }
   }, [submissionsStatus])
 
+  useEffect(() => {
+    if (lat && lng && zoom) {
+      pageDispatch(
+        setZoomAndLocation({
+          zoom,
+          location: {
+            lat,
+            lng,
+          },
+        }),
+      )
+      return
+    }
+    if (geoData?.coords) {
+      pageDispatch(
+        setZoomAndLocation({
+          zoom: 15,
+          location: {
+            lat: geoData?.coords?.latitude,
+            lng: geoData?.coords?.longitude,
+          },
+        }),
+      )
+    }
+  }, [stringify(geoData)])
+
   const isLoading = submissionsStatus === 'loading'
 
   return (
@@ -47,11 +81,7 @@ export const Homepage = () => {
           alert(JSON.stringify(data, null, 2))
         }}
       />
-      <Map
-        latitude={geoData?.coords?.latitude}
-        longitude={geoData?.coords?.longitude}
-        data={submissions}
-      />
+      <Map data={submissions} />
     </div>
   )
 }
