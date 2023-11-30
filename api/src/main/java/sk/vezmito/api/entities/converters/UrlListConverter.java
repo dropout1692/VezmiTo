e8ba8472791805c7c655f630bf18ee435000f52f
Michal Marusinec <michal.marusinec@scr.sk>
2023-11-30 21:05:34 +0100

    cert & NPE fix

--- api/src/main/java/sk/vezmito/api/entities/converters/UrlListConverter.java
+++ api/src/main/java/sk/vezmito/api/entities/converters/UrlListConverter.java
@@ -6,9 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UrlListConverter implements AttributeConverter<List<String>, String> {
+
     @Override
     public String convertToDatabaseColumn(List<String> strings) {
-        return String.join(",", strings);
+        if (strings.size() > 0) {
+            return String.join(",", strings);
+        } else {
+            return "";
+        }
     }
 
     @Override
